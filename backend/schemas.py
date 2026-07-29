6aa833f48c0167f131c4d423623abfdfc72f6279
24aeronyx <arielzakly24@gmail.com>
2026-07-29 10:17:00 +0800

    Hilangkan penambahan stok lama

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,13 +1,6 @@
 from pydantic import BaseModel
 from datetime import date
 from typing import List, Optional
-
-class StokObatBaru(BaseModel):
-    nama_obat: str
-    kategori: str = "Umum"
-    jumlah: int
-    tanggal_kedaluwarsa: date  # Otomatis tervalidasi ke format YYYY-MM-DD
-    gambar: Optional[str] = ""
     
 class ItemKeranjang(BaseModel):
     id_obat: int
@@ -16,17 +9,6 @@ class ItemKeranjang(BaseModel):
 class TransaksiKasir(BaseModel):
     kasir: str
     keranjang: List[ItemKeranjang]
-    
-class ItemObatMasuk(BaseModel):
-    nama_obat: str
-    kategori: str = "Umum"
-    harga_jual: int = 0
-    jumlah: int
-    tanggal_kedaluwarsa: date
-    gambar: Optional[str] = ""
-    
-class BulkStokObat(BaseModel):
-    items: List[ItemObatMasuk]
     
 class LoginRequest(BaseModel):
     username: str
